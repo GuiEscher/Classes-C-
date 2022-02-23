cd658a452d48ab6c13a0de9247f1a9ec842639e6
GuiEscher <99131269+GuiEscher@users.noreply.github.com>
2022-02-23 13:42:58 -0300

    Create main.cpp
    Usado para testagem da classe.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "classe.h"
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    Computador* C1=new Computador();
+    C1->imprimir();
+    //system("clear");
+    C1->abrir_app();
+    
+    C1->download_arquivo();
+    C1->excluir_arquivo();
+    C1->imprimir();
+    return 0;
+}
